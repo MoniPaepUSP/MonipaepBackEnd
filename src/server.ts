170dbe41aa211eea4556946f9f6a43eed6c2217e
DaviMoreira27 <davimsantana2706@gmail.com>
2025-02-09 01:29:21 -0300

    fix: fixed husky pre commit script

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import app from './app';
 import logger from './common/loggerConfig';
 
-app.listen(3333, () => logger.info('Server Running'))
+app.listen(3333, () => logger.info('Server Running'));
