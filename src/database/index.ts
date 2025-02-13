12663125eb7108382688e4ff6dc2590c8de9eea5
DaviMoreira27 <davimsantana2706@gmail.com>
2025-02-12 22:26:17 -0300

    refactor: adjusted typeorm initialization and added debug command

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,77 +1,25 @@
-// import {Connection, createConnection} from "typeorm"
-
-// export default async (): Promise<Connection> =>{
-//     return createConnection()
-// }
-
 import { DataSource } from 'typeorm';
-import fs from 'fs';
+import { dbConfig } from '../config';
+import logger from '../common/loggerConfig';
 
-const {
-  POSTGRES_HOST: HOST,
-  POSTGRES_HOST_FILE: HOST_FILE,
-  POSTGRES_USER: USER,
-  POSTGRES_USER_FILE: USER_FILE,
-  POSTGRES_PASSWORD: PASSWORD,
-  POSTGRES_PASSWORD_FILE: PASSWORD_FILE,
-  POSTGRES_DB: DB,
-  POSTGRES_DB_FILE: DB_FILE,
-
-  ENVIRONMENT_TYPE: ENVIRONMENT,
-} = process.env; // reading from environment all usefull variables
-
-// development
-let host = 'localhost';
-let user = 'pgdefault_user';
-let password = '2706';
-let database = 'monipaep';
-
-// production
-if (ENVIRONMENT == 'PRODUCTION') {
-  console.log('Entering production mode');
-
-  // reading docker secrets in case it runs on production mode
-  host = (HOST_FILE ? fs.readFileSync(HOST_FILE).toString() : HOST) ?? '';
-  user = (USER_FILE ? fs.readFileSync(USER_FILE).toString() : USER) ?? '';
-  password =
-    (PASSWORD_FILE
-      ? fs.readFileSync(PASSWORD_FILE, 'utf8').toString()
-      : PASSWORD) ?? '';
-  database = (DB_FILE ? fs.readFileSync(DB_FILE).toString() : DB) ?? '';
-} else {
-  console.log('Production: DEVELOPMENT');
-  console.log('Variables:');
-  console.log(host);
-  console.log(user);
-  console.log(password);
-  console.log(database);
-}
-
-// initializing database with typeorm
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: `${host}`,
-  port: 5432,
-  username: `${user}`,
-  password: `${password}`,
-  database: `${database}`,
-  logging: true,
+  host: dbConfig.DB_HOST,
+  port: Number(dbConfig.DB_PORT),
+  username: dbConfig.DB_USER,
+  password: dbConfig.DB_PASSWORD,
+  database: dbConfig.DB_NAME,
+  logging: Boolean(dbConfig.DB_LOGGING),
+  synchronize: Boolean(dbConfig.DB_SYNC),
   migrations: ['./src/database/migrations/**.ts'],
   entities: ['./src/models/**.{ts,js}'],
-  // "cli":{
-  //     "migrationsDir": "./src/database/migrations"
-  // }
 });
 
-// inciando banco de dados
 AppDataSource.initialize()
   .then(() => {
-    console.log('Data source typeorm initialized!asdasd');
-    // console.log(host);
-    // console.log(user);
-    // console.log(password);
-    // console.log(database);
+    logger.info('Data source typeorm initialized!');
+
   })
   .catch((err) => {
-    console.error('Error on data source typeorm intialization ', err);
+    logger.error('Error on data source typeorm intialization ', err);
   });
